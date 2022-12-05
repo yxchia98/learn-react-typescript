c10b74387da8e6c787c20fee02f6a59ae4719d06
lulmao <yxchia98@gmail.com>
2022-12-05 18:20:04 +0800

    updated files

--- apps/jbook/src/plugins/unpkg-path-plugin.ts
+++ apps/jbook/src/plugins/unpkg-path-plugin.ts
@@ -1,47 +1,36 @@
 import * as esbuild from "esbuild-wasm";
-import axios from "axios";
+import localforage from "localforage";
+
+const fileCache = localforage.createInstance({
+    name: "filecache",
+});
 
 export const unpkgPathPlugin = () => {
     return {
         name: "unpkg-path-plugin",
         setup(build: esbuild.PluginBuild) {
-            build.onResolve({ filter: /.*/ }, async (args: any) => {
-                console.log("onResolve", args);
-                if (args.path === "index.js") {
-                    return { path: args.path, namespace: "a" };
-                }
-                if (args.path.includes("./") || args.path.includes("../")) {
-                    return {
-                        namespace: "a",
-                        path: new URL(
-                            args.path,
-                            "https://unpkg.com" + args.resolveDir + "/"
-                        ).href,
-                    };
-                }
+            // handle root entry file of 'index.js'
+            build.onResolve({ filter: /(^index\.js$)/ }, () => {
+                return { path: "index.js", namespace: "a" };
+            });
+
+            // handle relative paths in a module
+
+            build.onResolve({ filter: /^\.+\// }, (args: any) => {
                 return {
                     namespace: "a",
-                    path: `https://unpkg.com/${args.path}`,
+                    path: new URL(
+                        args.path,
+                        "https://unpkg.com" + args.resolveDir + "/"
+                    ).href,
                 };
             });
 
-            build.onLoad({ filter: /.*/ }, async (args: any) => {
-                console.log("onLoad", args);
-
-                if (args.path === "index.js") {
-                    return {
-                        loader: "jsx",
-                        contents: `
-                        import React, {useState} from 'react';
-                        console.log(React, useState);
-                        `,
-                    };
-                }
-                const { data, request } = await axios.get(args.path);
+            // handle main file of a module
+            build.onResolve({ filter: /.*/ }, async (args: any) => {
                 return {
-                    loader: "jsx",
-                    contents: data,
-                    resolveDir: new URL("./", request.responseURL).pathname,
+                    namespace: "a",
+                    path: `https://unpkg.com/${args.path}`,
                 };
             });
         },
